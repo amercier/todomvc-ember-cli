df85459f78d52866a78cd1b44562da00278064df
Alexandre Mercier <amercier@slideshare.com>
2014-10-31 21:39:55 -0700

    Added todo state change

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+  isCompleted: function(key, value){
+    var model = this.get('model');
+
+    if (value === undefined) {
+      // property being used as a getter
+      return model.get('isCompleted');
+    } else {
+      // property being used as a setter
+      model.set('isCompleted', value);
+      model.save();
+      return value;
+    }
+  }.property('model.isCompleted')
 });
