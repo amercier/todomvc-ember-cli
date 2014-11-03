010234009b0c5c8c7f625035b4d6aa5cf042103b
Alexandre Mercier <amercier@slideshare.com>
2014-11-03 13:27:35 -0800

    Rewritten integration test using Promises

--- tests/integration/add-new-todo-test.js
+++ tests/integration/add-new-todo-test.js
@@ -1,20 +1,30 @@
 
 import Ember from 'ember';
 import { moduleForIntegation, container } from '../helpers/module-for-integration';
+import delay from '../helpers/delay';
 
 moduleForIntegation('Integration - Add a new todo');
 
-test('Typing a todo name and pressing ENTER adds a new todo', function() {
-  expect(2);
-  Ember.run(function () {
-    var text = 'My new awesome todo';
+var ms = 500;
 
-    visit('/');
-    fillIn('#new-todo', text);
-    keyEvent('#new-todo', 'keyup', 13);
-    andThen(function() {
+asyncTest('Typing a todo name and pressing ENTER adds a new todo', function() {
+  expect(2);
+
+  var text = 'My new awesome todo';
+
+  visit('/')
+    .then(delay(ms))
+    .then(function () {
+      return fillIn('#new-todo', text);
+    })
+    .then(delay(ms))
+    .then(function () {
+      return keyEvent('#new-todo', 'keyup', 13);
+    })
+    .then(delay(ms))
+    .then(function(msg) {
       equal(find('#main > ul > li').size(), 4);
       equal(find('#main > ul > li:nth-of-type(4) label').text(), text);
+      start(); // see http://api.qunitjs.com/QUnit.asyncTest/
     });
-  });
 });
