2b8269ae7891bddb33a6e01814ea3ff3b23895b2
Alexandre Mercier <amercier@slideshare.com>
2014-11-03 18:48:43 -0800

    Added unit tests for Todos controller

--- tests/1-unit/controllers/todos-test.js
+++ tests/1-unit/controllers/todos-test.js
@@ -1,7 +1,24 @@
+import Ember from 'ember';
 import {
   moduleFor,
   test
 } from 'ember-qunit';
+import FIXTURES from '../../fixtures/todo';
+
+function mockModel(data) {
+  return data.map(function(item) {
+    return Ember.ObjectProxy.create(Ember.merge(item, {
+      saved: false,
+      save: function() {
+        this.set('saved', true);
+      },
+      deleted: false,
+      deleteRecord: function() {
+        this.set('deleted', true);
+      }
+    }));
+  });
+}
 
 moduleFor('controller:todos', 'TodosController', {
   // Specify the other units that are required for this test.
@@ -12,4 +29,36 @@ moduleFor('controller:todos', 'TodosController', {
 test('it exists', function() {
   var controller = this.subject();
   ok(controller);
+});
+
+
+test('remaining', function () {
+  expect(2);
+  var controller = this.subject();
+  var models = mockModel(FIXTURES);
+  controller.addObjects(models);
+  equal(controller.get('remaining'), 1);
+  controller.findBy('id', '3').set('isCompleted', true);
+  equal(controller.get('remaining'), 0);
+});
+
+
+test('completed', function () {
+  expect(2);
+  var controller = this.subject();
+  var models = mockModel(FIXTURES);
+  controller.addObjects(models);
+  equal(controller.get('completed'), 2);
+  controller.findBy('id', '3').set('isCompleted', true);
+  equal(controller.get('completed'), 3);
+});
+
+test('allAreDone', function() {
+  expect(2);
+  var controller = this.subject();
+  var models = mockModel(FIXTURES);
+  controller.addObjects(models);
+  equal(controller.get('allAreDone'), false);
+  controller.findBy('id', '3').set('isCompleted', true);
+  equal(controller.get('allAreDone'), true);
 });
