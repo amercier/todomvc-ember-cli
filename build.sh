7ba5f47bc5914d453e4f14ea348fdb93c3cd8463
Alex Mercier <pro.alexandre.mercier@gmail.com>
2015-01-14 11:55:50 -0800

    Fixed an incompatibility with Linux's sed

--- build.sh
+++ build.sh
@@ -2,10 +2,6 @@
 
 DIR=dist
 
-function sed-in-place {
-  sed -i '' "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)\<\>/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
-}
-
 if [ $# -ne 2 ]; then
   echo "Syntax: $0 DIR BASEURL" 2>&1
   exit 1
@@ -23,7 +19,13 @@ ember build --output-path="$1"
 search='<base href="/" />'
 replace='<base href="'$2'/" />'
 
-sed -i '' "s#$search#$replace#" "$1/index.html"
-sed -i '' "s#$search#$replace#" "$1/tests/index.html"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # MacOS
+  sed -i '' -e "s#$search#$replace#" "$1/index.html"
+  sed -i '' -e "s#$search#$replace#" "$1/tests/index.html"
+else
+  sed -i -e "s#$search#$replace#" "$1/index.html"
+  sed -i -e "s#$search#$replace#" "$1/tests/index.html"
+fi
 
 echo " " > "$1/.nojekyll"
